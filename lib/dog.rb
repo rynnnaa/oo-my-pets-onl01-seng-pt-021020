47a227ff1785bac683f8a86a832c288f9ddcf0e1
Ryna Truong <rynatruong@outlook.com>
2020-03-18 18:02:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :owner
   attr_reader :name
   
-  def initialize(name, owner, mood)
+  def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
