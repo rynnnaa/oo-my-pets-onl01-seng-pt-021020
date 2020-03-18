bcf021c5f098f65843999c31d2f63f59f281e026
Ryna Truong <rynatruong@outlook.com>
2020-03-18 18:32:03 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,71 @@
 class Owner
-  # code goes here
+  
+  @@all = []
+
+  attr_reader :name, :species
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  
+  def say_species
+    return "I am a #{@species}."
+    end
+    
+  def self.all 
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all
+    @@all = []
+  end
+  
+  def cats
+    Cat.all.select{|cat|cat.owner == self}
+  end
+  
+  def dogs 
+    Dog.all.select{|dog|dog.owner == self}
+  end
+  
+  def buy_cat(cat)
+    Cat.new(cat, self)
+  end
+  
+  def buy_dog(dog)
+    Dog.new(dog, self)
+  end
+  
+  def walk_dogs
+    dogs.each do|dog|
+      dog.mood = "happy"
+    end
+  end
+  
+  def feed_cats
+    cats.each do|cat|
+      cat.mood = "happy"
+    end
+  end
+  
+  def sell_pets
+    pets = dogs + cats
+    pets.each do |pet|
+      pet.mood ="nervous"
+      pet.owner = nil
+    end
+  end
+  
+  def list_pets
+    pets = dogs + cats
+    pets.each do |pet|
+      return "I have #{dogs.length} dog(s), and #{cats.length} cat(s)."
+    end
+  end
 end
